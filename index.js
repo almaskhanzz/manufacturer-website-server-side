c2f6f9ba02c42e3432d9c61a84a24ad1b5693157
Almas Khan <almas.rbm@gmail.com>
2022-05-28 13:27:27 +0600

    role for user and admin updated

--- index.js
+++ index.js
@@ -62,9 +62,20 @@ async function run() {
         })
 
         //all users
-        app.get('/user', verifyJWT, async (req, res) => {
+        app.get('/user', async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
+        })
+
+        //role for user .....admin
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
         })
 
         //for user authentication
